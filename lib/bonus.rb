cd425bfcba6a7dd8d582a2a59c17ff1da0819261
Ola Enol <eniolar10@gmail.com>
2020-02-04 00:24:57 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -29,5 +29,6 @@ epic_tragedy[:capulet][:heroine][:status] = "Dead"
 puts epic_tragedy
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+  
   epic_tragedy
 end
